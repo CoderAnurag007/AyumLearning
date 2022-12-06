5362c592a105777f36747b4e2112c76f25ddb689
Anurag <anuragsinghg99@gmail.com>
2022-12-06 17:25:05 +0530

    added popup

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -5,12 +5,14 @@ import Faq from "./Faqs/Faq";
 import Footer from "./Footer/Footer";
 import Land from "./LandPage/Land";
 import Nav from "./Navbar/Nav";
+import Pop from "./Popup/Pop";
 import Team from "./Team/Team";
 
 const HomeComp = () => {
   return (
     <>
       <Nav />
+      <Pop />
       <Land />
       <Course />
       <Certificate />
---
+++ Components/Popup/Pop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const Pop = () => {
+  return (
+    <>
+      <div className={`${styles.popupbox}`}>
+        Hurry! Limited Slots are available for Winter's Batch
+      </div>
+    </>
+  );
+};
+
+export default Pop;
